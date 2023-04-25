dbcd8486eaf2aee850dc2d110d74d8b279e3333e
MiqueLDG <miquelduran@iesmontsia.org>
2023-04-25 19:24:04 +0200

    miquel

--- ProgramaInicial.java
+++ ProgramaInicial.java
@@ -7,12 +7,16 @@ import Teatregrama.*;
 
 public class ProgramaInicial {
     public static void main(String[] args) {
+        String[][] butaca = new String[10][10];
         Scanner scanner = new Scanner(System.in);
         LocalDate data = LocalDate.now();
         String dataFormatada;
-        LocalTime horaInici;
+        String horaInici = "";
         int numEspectadors;
         float preu = 0;
+        int columna;
+        int fila;
+        int edat = 0;
 
         // Crear obra
         System.out.println("Introdueix les dades de l'obra:");
@@ -38,13 +42,13 @@ public class ProgramaInicial {
             dataFormatada = data.format(dateFormatter);
             System.out.println("Dia (dd/mm/aaaa): " + dataFormatada);
             System.out.print("Hora d'inici (format 24 hores, hh:mm): ");
-            String horaIniciString = scanner.nextLine();
-            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
-            horaInici = LocalTime.parse(horaIniciString, timeFormatter);
+            horaInici = scanner.nextLine();
+            //DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+            //horaInici = LocalTime.parse(horaIniciString, timeFormatter);
             System.out.print("Preu de l'entrada: ");
             preu = scanner.nextFloat();
             scanner.nextLine(); // Consumir la línia en blanc
-            sessions[i] = new Sessio(dataFormatada, horaInici, preu);
+            sessions[i] = new Sessio(dataFormatada, preu);
         }
 
         // Crear espectadors
@@ -52,42 +56,119 @@ public class ProgramaInicial {
         numEspectadors = scanner.nextInt();
         scanner.nextLine(); // Consumir la línia en blanc
         Espectador[] espectadorsInserits = new Espectador[numEspectadors];
+        Espectador[] espectadorsAdults = new Espectador[numEspectadors]; // Nuevo arreglo para espectadores mayores de edad
+        int numEspectadorsAdults = 0; // Nuevo contador para llevar la cuenta de espectadores mayores de edad
         for (int i = 0; i < numEspectadors; i++) {
             System.out.println("Introdueix les dades de l'espectador " + (i + 1) + ":");
             System.out.print("Nom: ");
             String nom = scanner.nextLine();
             System.out.print("Edat: ");
-            int edat = scanner.nextInt();
+            edat = scanner.nextInt();
             System.out.print("Diners que té: ");
             float diners = scanner.nextFloat();
             scanner.nextLine(); // Consumir la línia en blanc
+            
+            
+            if (diners < preu){
+            System.out.println("No té diners suficients per pagar l'entrada");
+                continue;
+            }
             Espectador espectador = new Espectador(nom, edat, diners);
-            espectadorsInserits[i] = espectador;
             if (edat >= 18 || !perAdults) { // Comprobar si el espectador es mayor de edad y si la obra es para mayores de 18 años
-                espectadorsInserits[i] = espectador;
-                // Aplicar el preu de l'entrada al diner de l'espectador
+                espectadorsAdults[numEspectadorsAdults] = espectador; // Agregar al arreglo de espectadores mayores de edad
+                numEspectadorsAdults++;
                 diners = diners - preu;
-                System.out.println("Ara te queden " + diners + "€");
+                System.out.println("Ara te queden " + diners + "€"); // Incrementar el contador de espectadores mayores de edad
+                // Aplicar el preu de l'entrada al diner de l'espectador
+                // Aplicar el preu de l'entrada al diner de l'espectador
+                
             } else {
                 System.out.println("L'espectador " + nom + " no pot veure aquesta obra.");
             }
+            espectadorsInserits[i] = espectador;
         }
+        System.out.println("Posicio Butaca: ");
+        if (perAdults = true) {
+    for (int j = 0; j < espectadorsInserits.length; j++) {
+        while (true) {
+            System.out.print("Fica una fila entre el valor 0 i " + (butaca.length - 1) + "): ");
+            fila = scanner.nextInt();
+            System.out.print("Fica una columna entre el valor 0 i  " + (butaca[0].length - 1) + "): ");
+            columna = scanner.nextInt();
+            if (fila >= 0 && fila < butaca.length && columna >= 0 && columna < butaca[0].length) {
+                if (butaca[fila][columna] != null) {
+                    System.out.println("¡La butaca ya está ocupada!");
+                    continue; // Reiniciamos el bucle sin ejecutar la asignación
+                }
+                if (edat >= 18) {
+                    butaca[fila][columna] = String.valueOf(j+1);
+                    break; // Salimos del bucle si la asignación ha sido exitosa
+                }
+            } else {
+                System.out.println("Ubicación inválida.");
+                continue;
+            }
+        }
+    }
+} else if(perAdults = false){
+    for (int j = 0; j < espectadorsInserits.length; j++) {
+        while (true) {
+            System.out.print("Fica una fila entre el valor 0 i " + (butaca.length - 1) + "): ");
+            fila = scanner.nextInt();
+            System.out.print("Fica una columna entre el valor 0 i  " + (butaca[0].length - 1) + "): ");
+            columna = scanner.nextInt();
+
+            if (fila >= 0 && fila < butaca.length && columna >= 0 && columna < butaca[0].length) {
+                if (butaca[fila][columna] != null) {
+                    System.out.println("¡La butaca ya está ocupada!");
+                    continue; // Reiniciamos el bucle sin ejecutar la asignación
+                }
+                butaca[fila][columna] = String.valueOf(j+1);
+                break; // Salimos del bucle si la asignación ha sido exitosa
+            } else {
+                System.out.println("Ubicación inválida.");
+                continue;
+            }
+        }
+    }
+}
 
         for (Sessio sessio : sessions) {
             dataFormatada = sessio.getDia();
             dataFormatada = data.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
-            horaInici = sessio.getHora();
             System.out.println("Llista d'espectadors per a la sessió del " + dataFormatada + " a les " + horaInici + ":");
-            Espectador[] espectadorsSessio = sessio.getEspectadors();
-            if (espectadorsSessio != null) {
-                for (Espectador espectador : espectadorsSessio) {
-                    System.out.println("- " + espectador.getNom() + " (" + espectador.getEdat() + " anys)");
-                }
-            } else {
-                System.out.println("- No hi ha espectadors per a aquesta sessió");
-            }
-            System.out.println();
+            
+            
 
         }
+        if (perAdults){
+            for (int i = 0; i < numEspectadorsAdults; i++) {
+                System.out.println("- " + espectadorsAdults[i].getNom() + " (" + espectadorsAdults[i].getEdat() + " anys)");
+              }
+        }
+
+        else if(espectadorsInserits != null) {
+            System.out.println("Els espectadors introduïts són:");
+            for (int i = 0; i < numEspectadors; i++) {
+                System.out.println("- " + espectadorsInserits[i].getNom() + " (" + espectadorsInserits[i].getEdat() + " anys)");
+              }
+    } else{
+        System.out.println("- No hi ha espectadors per a aquesta sessió");
     }
+    System.out.println();
+    System.out.println("Butaca ocupada o sense ocupar: ");
+
+    for(fila = 0;fila<butaca.length;fila++)
+    {
+        for ( columna = 0; columna < butaca[fila].length; columna++) {
+            if (butaca[fila][columna] != null) {
+                System.out.print("X ");
+            } else {
+                System.out.print("O ");
+            }
+        }
+        System.out.println();
+    }
+}
+
 }
